1959b1b8cbf4893e7986a7e427cd823218fe5947
SharonC <sharon.s.c.chien@gmail.com>
2019-04-08 23:28:17 -0400

    added figlet title

--- index.js
+++ index.js
@@ -2,30 +2,52 @@ const inquirer = require("inquirer")
 let customer = require("./bamazonCustomer.js")
 let manager = require("./bamazonManager.js")
 let supervisor = require("./bamazonSupervisor.js")
+let figlet = require("figlet")
 
 
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "Who are you?",
-            choices: ["Customer","Manager","Supervisor","EXIT"],
-            name: "id"
+let index = {
+    start: () => {
+        figlet.text('Welcome to Bamazon', {
+            font: "Double",
+            horizontalLayout: 'default',
+            verticalLayout: 'default'
+        }, function(err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
             }
-    ]).then( answer => {
-        switch (answer.id) {
-            case "Customer":
-                customer.menu();
-                break;
-            case "Manager":
-                manager.menu();
-                break;
-            case "Supervisor":
-                supervisor.menu();
-                break;        
-            case "EXIT":
-                process.exit();
-                break;
-            default:
-                break;
-        }
-    })
+            console.log(data);
+            index.menu()
+        });
+    },
+    menu: () => {
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Who are you?",
+                choices: ["Customer","Manager","Supervisor","EXIT"],
+                name: "id"
+                }
+        ]).then( answer => {
+            switch (answer.id) {
+                case "Customer":
+                    customer.menu();
+                    break;
+                case "Manager":
+                    manager.menu();
+                    break;
+                case "Supervisor":
+                    supervisor.menu();
+                    break;        
+                case "EXIT":
+                    process.exit();
+                    break;
+                default:
+                    break;
+            }
+        })
+    }
+}
+
+index.start();
